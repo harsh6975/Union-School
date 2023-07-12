b00316c9b70cfa1cc820ce7bf0e330c743e6890c
harsh6975 <harshu1234ashv@gmail.com>
2023-07-12 23:15:17 +0530

    contact

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -11,79 +11,77 @@ function Contact() {
     <div>
       <Navbar1 />
       <Banner image={contact} />
-      <div className={styles.content6}>
-        <section className={styles.middlesection}>
-          <div className={styles.box}>
-            <h1>CONTACT US</h1>
-            <img height="120" width="600" src={design} alt="design" />
-            <div className={styles.boxx}>
-              <h4>
-                Union School Newtown <br />
-                Bhagowanpur , Newtown ( Hatishala ) <br />
-                Kol-700135 , W.B. <br />
-                P: +91 77186 - 11100 | +91 6297281161 <br />
-                Mail: info@unionschool.org / unionschoolnt@gmail.com
-              </h4>
-            </div>
-
-            <div className={styles.boxx1}>
-              <h4>CAMPUS MAP</h4>
-              <img src="images/MAPLOGO.png" alt="" />
-            </div>
-            <img className={styles.I} src="images/map.png" alt="" />
+      <div className={styles.middlesection}>
+        <div className={styles.box}>
+          <h1>CONTACT US</h1>
+          <img height="120" width="600" src={design} alt="design" />
+          <div className={styles.boxx}>
+            <h4>
+              Union School Newtown <br />
+              Bhagowanpur , Newtown ( Hatishala ) <br />
+              Kol-700135 , W.B. <br />
+              P: +91 77186 - 11100 | +91 6297281161 <br />
+              Mail: info@unionschool.org / unionschoolnt@gmail.com
+            </h4>
           </div>
-          <div className={styles.boxcontainer}>
-            <div className={styles.box1}>
-              <h1>CONTACT FORM</h1>
-            </div>
-            <form action="">
-              <div className={styles.formcontainer}>
-                <div className={styles.formcontrol}>
-                  <label for="first-name">First Name</label>
-                  <input
-                    id="first-name"
-                    name="first-name"
-                    placeholder="First Name"
-                    type="text"
-                  />
-                </div>
 
-                <div className={styles.formcontrol}>
-                  <label for="last-name">Last Name</label>
-                  <input
-                    id="last-name"
-                    name="last-name"
-                    placeholder="Last Name"
-                    type="text"
-                  />
-                </div>
-
-                <div className={styles.formcontrol}>
-                  <label for="email">Email Addrss*</label>
-                  <input type="email" id="email" name="email" />
-                </div>
-
-                <div className={styles.formcontrol}>
-                  <label for="phone-no">Phone Number*</label>
-                  <input type="number" id="phone-no" name="phone-no" />
-                </div>
-
-                <div className={styles.control}>
-                  <label for="comment">Questions / Comments</label>
-                  <textarea
-                    name="comment"
-                    id="comment"
-                    cols="30"
-                    rows="4"
-                  ></textarea>
-                </div>
-              </div>
-              <div className={styles.button_container}>
-                <button type="submit">Apply Now</button>
-              </div>
-            </form>
+          <div className={styles.boxx1}>
+            <h4>CAMPUS MAP</h4>
+            <img src="images/MAPLOGO.png" alt="" />
           </div>
-        </section>
+          <img className={styles.I} src="images/map.png" alt="" />
+        </div>
+        <div className={styles.boxcontainer}>
+          <div className={styles.box1}>
+            <h1>CONTACT FORM</h1>
+          </div>
+          <form action="">
+            <div className={styles.formcontainer}>
+              <div className={styles.formcontrol}>
+                <label for="first-name">First Name</label>
+                <input
+                  id="first-name"
+                  name="first-name"
+                  placeholder="First Name"
+                  type="text"
+                />
+              </div>
+
+              <div className={styles.formcontrol}>
+                <label for="last-name">Last Name</label>
+                <input
+                  id="last-name"
+                  name="last-name"
+                  placeholder="Last Name"
+                  type="text"
+                />
+              </div>
+
+              <div className={styles.formcontrol}>
+                <label for="email">Email Addrss*</label>
+                <input type="email" id="email" name="email" />
+              </div>
+
+              <div className={styles.formcontrol}>
+                <label for="phone-no">Phone Number*</label>
+                <input type="number" id="phone-no" name="phone-no" />
+              </div>
+
+              <div className={styles.control}>
+                <label for="comment">Questions / Comments</label>
+                <textarea
+                  name="comment"
+                  id="comment"
+                  cols="30"
+                  rows="4"
+                ></textarea>
+              </div>
+            </div>
+            <div className={styles.button_container}>
+              <button type="submit">Apply Now</button>
+            </div>
+          </form>
+        </div>
       </div>
       <Footer />
     </div>
