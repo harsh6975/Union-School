0622343310136ab850f839dd9eb23711674b2a8f
harsh6975 <harshu1234ashv@gmail.com>
2023-07-12 15:22:02 +0530

    miniNavbar added

---
+++ src/component/MiniNavbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MiniNavbar() {
+  return (
+    <header>
+      <h1>UNION SCHOOL NEWTOWN</h1>
+      <ul className="link-container">
+        <li>FB</li>
+        <li>TW</li>
+        <li>Insta</li>
+        <li>Search</li>
+      </ul>
+    </header>
+  );
+}
+
+export default MiniNavbar;
