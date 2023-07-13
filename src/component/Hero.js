eca558e709e245a92495772b5ad272bd7832d59b
harsh6975 <harshu1234ashv@gmail.com>
2023-07-14 01:04:57 +0530

    hero modified

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from "../styles/hero.module.css";
 import hero1 from "../images/hero1.png";
 import hero2 from "../images/hero2.png";
 import hero3 from "../images/hero3.png";
@@ -9,7 +8,7 @@ import ImageSlider from "./Image Slider/ImageSlider";
 const images = [hero1, hero2, hero3];
 function Hero() {
   return (
-    <div className={styles.container}>
+    <div>
       <ImageSlider
         images={images}
         autoPlay={true}
