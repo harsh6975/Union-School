07b54f946f33d572c8983aba18cd2524ea36e4e9
harsh6975 <harshu1234ashv@gmail.com>
2023-07-13 23:42:38 +0530

    hero with image slider

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from "../styles/hero.module.css";
-import heroicon from "../images/heroicon.png";
+import hero1 from "../images/hero1.png";
+import hero2 from "../images/hero2.png";
+import hero3 from "../images/hero3.png";
 
+import ImageSlider from "./Image Slider/ImageSlider";
+
+const images = [hero1, hero2, hero3];
 function Hero() {
   return (
-    <div className={styles.align}>
-      <img className={styles.h} src={heroicon} alt="" />
+    <div className={styles.container}>
+      <ImageSlider
+        images={images}
+        autoPlay={true}
+        maxImg={1}
+        imageName={"hero"}
+      />
     </div>
   );
 }
